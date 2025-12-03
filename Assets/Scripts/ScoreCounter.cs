8a369c996c084464d7fb331e15246084f487d23e
Twan-Solo <160876469+Twan-Solo@users.noreply.github.com>
2025-12-03 17:13:57 -0500

    Update ScoreCounter.cs

--- Assets/Scripts/ScoreCounter.cs
+++ Assets/Scripts/ScoreCounter.cs
@@ -7,7 +7,7 @@ public class ScoreCounter : MonoBehaviour
     private int score = 0;
 
     public TextMeshProUGUI ScoreText;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Start is called before the first frame update
     void Start()
     {
         
@@ -20,7 +20,6 @@ public class ScoreCounter : MonoBehaviour
     }
     void Awake()
     {
-        // Assign the singleton instance
         if (Instance == null)
         {
             Instance = this;
